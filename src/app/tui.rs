a78f035576bf56cb669679b56ecce5c9d527e27a
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2024-07-30 00:16:30 +0530

    Formats code
    Signed-off-by: Saahil Bhavsar <saahil_bhavsar@outlook.com>

--- src/app/tui.rs
+++ src/app/tui.rs
@@ -16,13 +16,17 @@ pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 pub fn init() -> io::Result<Tui> {
     execute!(stdout(), EnterAlternateScreen)?;
     execute!(stdout(), Clear(ClearType::All))?;
+
     enable_raw_mode()?;
+
     Terminal::new(CrosstermBackend::new(stdout()))
 }
 
 pub fn restore() -> io::Result<()> {
     execute!(stdout(), LeaveAlternateScreen)?;
     execute!(stdout(), Clear(ClearType::All))?;
+
     disable_raw_mode()?;
+
     Ok(())
 }
